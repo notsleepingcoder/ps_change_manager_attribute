0ce7659164e8a5dbdc5cfa89fa630b504be13c2a
noname dude <73312551+notsleepingcoder@users.noreply.github.com>
2024-08-14 19:42:34 +0300

    Add files via upload

---
+++ change_manager_attribute.ps1
@@ -0,0 +1,16 @@
+﻿$MANAGER1=Get-ADUser -searchbase "OU=Office,OU=Russia,DC=headquarter,DC=company,DC=lpp,DC=com,DC=pl" -filter * -Properties * | ` 
+#Вместо [OU] подставляем Organization Unit из AD где будут искаться пользователи, например "OU=Office,OU=Russia,DC=headquarter,DC=company,DC=lpp,DC=com,DC=pl"
+where {$_.SamAccountName -like '[USER0]' -or `
+$_.SamAccountName -like '[USER1]' -or ` #Вместо [USER№] подставляем sAMAccountname из AD
+$_.SamAccountName -like '[USER2]' -or `
+$_.SamAccountName -like '[USER3]' -or `
+$_.SamAccountName -like '[USER4]' -or `
+$_.SamAccountName -like '[USER5]'
+}
+ForEach ($User1 in $MANAGER1)
+{
+$SAN1=$Ussaddasdasder.SamAccountName
+Set-ADUser -Identity $SAN1 -manager [MANAGER_NAME] #Тут вместо [MANAGER_NAME] подставляем sAMAccountname руководителя/менеджера
+Get-ADUser -Identity $SAN1 -Properties name, manager | select name, manager | ft
+}
+

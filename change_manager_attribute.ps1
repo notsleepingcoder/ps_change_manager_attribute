a3869ff5692aba1e91d81cb35a4584f7f4f40a1f
noname dude <73312551+notsleepingcoder@users.noreply.github.com>
2024-08-15 12:21:07 +0300

    Update change_manager_attribute.ps1

--- change_manager_attribute.ps1
+++ change_manager_attribute.ps1
@@ -1,16 +1,20 @@
-﻿$MANAGER1=Get-ADUser -searchbase "OU=Office,OU=Russia,DC=headquarter,DC=company,DC=lpp,DC=com,DC=pl" -filter * -Properties * | ` 
-#Вместо [OU] подставляем Organization Unit из AD где будут искаться пользователи, например "OU=Office,OU=Russia,DC=headquarter,DC=company,DC=lpp,DC=com,DC=pl"
+#change "OU=" to Organization Unit you need and "DC=" to your full domain controller address
+$MANAGER1=Get-ADUser -searchbase "OU=Office,OU=Russia,DC=headquarter,DC=company,DC=lpp,DC=com,DC=pl" -filter * -Properties * | ` 
+
+#Instead of [USER№] use user's sAMAccountname from Active Directory (use as much users as you need)
 where {$_.SamAccountName -like '[USER0]' -or `
-$_.SamAccountName -like '[USER1]' -or ` #Вместо [USER№] подставляем sAMAccountname из AD
-$_.SamAccountName -like '[USER2]' -or `
-$_.SamAccountName -like '[USER3]' -or `
-$_.SamAccountName -like '[USER4]' -or `
-$_.SamAccountName -like '[USER5]'
+  $_.SamAccountName -like '[USER1]' -or `
+  $_.SamAccountName -like '[USER2]' -or `
+  $_.SamAccountName -like '[USER3]' -or `
+  $_.SamAccountName -like '[USER4]' -or `
+  $_.SamAccountName -like '[USER5]'
 }
+
 ForEach ($User1 in $MANAGER1)
 {
 $SAN1=$Ussaddasdasder.SamAccountName
-Set-ADUser -Identity $SAN1 -manager [MANAGER_NAME] #Тут вместо [MANAGER_NAME] подставляем sAMAccountname руководителя/менеджера
+#instead of [MANAGER_NAME] use manager's sAMAccountname from Active Directory
+Set-ADUser -Identity $SAN1 -manager [MANAGER_NAME]
 Get-ADUser -Identity $SAN1 -Properties name, manager | select name, manager | ft
 }
 
